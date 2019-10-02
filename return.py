8d19a0dc2c8516bb4cec8194e397ae1260e3f492
mitisit89 <mitisit89@gmail.com>
2019-10-02 15:35:03 +0300

    hello

---
+++ return.py
@@ -0,0 +1,11 @@
+def maximum(x,y):
+    if x>y:
+        return x
+    elif x==y:
+        return 'Равные числа'
+    else :
+        return y
+
+print(maximum(2, 5))
+print(maximum(5, 5))
+print(maximum(4, 2))
